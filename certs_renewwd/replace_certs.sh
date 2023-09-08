95b740d888f73928f719bfa1f507da6da6246896
root <root@ip-172-31-22-184.eu-central-1.compute.internal>
2023-09-08 08:51:03 +0000

    ok

---
+++ certs_renewwd/replace_certs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cp -rp ../certs/web2.cipensoio.com.pem ../certs/web2.cipensoio.com.crt
+cp -rp ../certs/web2.cipensoio.com.crt_new .
+mv web2.cipensoio.com.crt_new web2.cipensoio.com.crt
+cp -rp ../certs/web2.cipensoio.com.crt /etc/ssl/certs/
+
+cp -rp ../certs/web4.cipensoio.com.pem ../certs/web4.cipensoio.com.crt
+mv web4.cipensoio.com.crt_done web4.cipensoio.com.crt
+cp -rp ../certs/web4.cipensoio.com.crt /etc/ssl/certs_nginx/
+
+systemctl reload apache2
+
+systemctl restart nginx
