7a34145948f185e5a79306cdcd78911f86881f6c
root <root@ip-172-31-22-184.eu-central-1.compute.internal>
2023-09-13 09:06:24 +0000

    nginx

--- certs_renewwd/replace_certs.sh
+++ certs_renewwd/replace_certs.sh
@@ -8,6 +8,11 @@ cp -rp ../certs/web4.cipensoio.com.pem ../certs/web4.cipensoio.com.crt
 mv web4.cipensoio.com.crt_done web4.cipensoio.com.crt
 cp -rp ../certs/web4.cipensoio.com.crt /etc/ssl/certs_nginx/
 
+cp -rp ../certs/web5.cipensoio.com.pem ../certs/web5.cipensoio.com.crt
+mv web5.cipensoio.com.crt_done web5.cipensoio.com.crt
+cp -rp ../certs/web5.cipensoio.com.crt /etc/ssl/certs_nginx/
+
+
 systemctl reload apache2
 
 systemctl restart nginx
---
+++ certs_renewwd/replace_certs_tomcat.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp -rp ../certs/web6.cipensoio.com.pem ../certs/web6.cipensoio.com.crt
+mv web6.cipensoio.com.crt_done web6.cipensoio.com.crt
+
+cp -rp ../certs/web7.cipensoio.com.pem ../certs/web7.cipensoio.com.crt
+mv web7.cipensoio.com.crt_done web7.cipensoio.com.crt
+
